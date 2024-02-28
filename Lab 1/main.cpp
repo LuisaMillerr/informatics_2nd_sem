357dba108aa5511a39d1283690f8c264010238d2
Luisa Miller <159237192+LuisaMillerr@users.noreply.github.com>
2024-02-28 15:52:28 +0300

    Update main.cpp

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <cmath>
-#include <limits>
+#include <cmath> //to work with area & coordinates
+#include <limits> //to check input
+#include <cstdio> //to use gets()
+#include <string>
 #define int long long
 using namespace std;
 
@@ -8,7 +10,7 @@ const double PI = 2 * asin(1.0);
 int polygonsNum = 1;
 
 struct StructCoordinates{
-    double coordinates[2];
+    double vertexCoordinates[2];
 };
 
 struct StructPolygon{
@@ -24,13 +26,15 @@ void inf_delete();
 double calculate_area(const double sideLength, const int sidesNum);
 double calculate_perimeter(const double sideLength, const int sidesNum);
 double *find_coordinates(StructPolygon &RegularPolygon);
-StructPolygon *rewrite_inf(StructPolygon &allPolygons, int polygonsNum);
-void exit_checking();
+StructPolygon *rewrite_inf(StructPolygon *allPolygons, int polygonsNum);
+void input_and_exit_checking(string &str);
+void draw_menu();
 
 signed main(){
+    string temp_str;
     StructPolygon *allPolygons = new StructPolygon[1];
     do{
-
+        input_and_exit_checking(temp_str);
     }while(true);
     delete [] allPolygons;
     return 0;
@@ -72,10 +76,35 @@ double calculate_area(const double sideLength, const int sidesNum){
     return (pow(sideLength, 2) * sidesNum / (4 * tan(PI/sidesNum)));
 }
 
-double *find_coordinates(StructPolygon &RegularPolygon){
+/*double *find_coordinates(StructPolygon &RegularPolygon){
 
+}*/
+
+StructPolygon *rewrite_inf(StructPolygon *allPolygons, const int polygonsNum){
+    StructPolygon *newAllPolygons = new StructPolygon[polygonsNum + 1];
+    for(int i = 0; i < polygonsNum; ++i){
+        newAllPolygons[i] = allPolygons[i];
+    }
+    delete [] allPolygons;
+    return (newAllPolygons);
 }
 
-StructPolygon *rewrite_inf(StructPolygon &allPolygons, int &polygonsNum){
+void exit_checking(string &str){
+    cin >> str;
+    cout << "Would you lke to continue?";
+    if(str == "exit" || str == "Exit"){
+        cout << "Are you sure you'd like to ext?";
+        str = "";
+        cin >> str;
+        if(str == "Yes" || str == "yes" || str == "yep" || str == "Yep"){
+            exit(0);
+        }
+    }
+    else{
+        cout << "OK, let's write a new polygon." << endl;
+    }
+}
+
+void draw_menu(){
 
 }
